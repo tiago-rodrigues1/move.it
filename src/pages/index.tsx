6a1ae47d881a83a707d03a59a1151c5012011294
tiago-rodrigues1 <tiagords127@gmail.com>
2021-03-13 11:00:35 -0300

    Adiciona > Head e title > Componente CompletedChallenges > Componente Countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
+import Head from 'next/head'
+
 //Componentes
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
@@ -8,11 +12,17 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
