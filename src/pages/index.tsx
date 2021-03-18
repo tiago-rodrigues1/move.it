d082f80d50ab85a92fd40f3363463abca206dcd6
tiago-rodrigues1 <tiagords127@gmail.com>
2021-03-18 14:38:43 -0300

    ChallengeBox no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 
 //Componentes
-import { CompletedChallenges } from "../components/CompletedChallenges";
-import { Countdown } from "../components/Countdown";
-import { ExperienceBar } from "../components/ExperienceBar";
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
+
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -24,7 +26,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
 
     </div>
