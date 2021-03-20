a0575371f0fe225fa9cdf5396dc8b39b97fa832c
tiago-rodrigues1 <tiagords127@gmail.com>
2021-03-20 14:27:35 -0300

    Adiciona função de iniciar novo ciclo

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react';
+import {ChallengesContext} from '../contexts/ChallengesContext'
 
-import styles from '../styles/components/Countdown.module.css'
+import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+    const { startNewChallenge } = useContext(ChallengesContext)
+
     const [time, setTime] = useState(0.1 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
@@ -33,6 +36,7 @@ export function Countdown() {
         } else if(isActive && time === 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time]);
 
